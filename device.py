a4db12ba5ff0d3b8de4e13ef43d454260b695dfc
Adrian Roman <aroman@valeo-it.com>
2022-07-04 20:02:40 +0300

    updated device class

--- device.py
+++ device.py
@@ -1,16 +1,25 @@
 class Device():
     cheapest = None
+
     def __init__(self,tip, firma, pret) -> None:
         self.tip = tip
         self.firma = firma
         self.pret = pret
         if Device.cheapest is None:
             Device.cheapest = pret
-        if pret < Device.cheapest:
+                   
+        elif pret < Device.cheapest:
             Device.cheapest = pret
 
     def describe(self):
         print("Acesta este un {}, produs de {}, cu pretul de {} de lei.")
+
+    def apply_discount(self, percentage):
+        self.percentage = percentage
+        result = (percentage * self.pret) / 100
+        final_result = self.pret - result
+        self.pret = round(final_result)
+        return round(self.pret)
 
     @classmethod
     def get_lowest_price(cls):
@@ -20,7 +29,11 @@ class Device():
 unu = Device("televizor", "panasonic", 4200)
 doi = Device("telefon", "samsung", 2000)
 trei = Device("monitor", "asus", 700)
-describes = [unu, doi, trei]
-print(Device.get_lowest_price())
-print(unu.describe())
-print(unu.firma)
+
+unu.apply_discount(10)
+doi.apply_discount(30)
+trei.apply_discount(15)
+
+print(unu.pret)
+print(doi.pret)
+print(trei.pret)
