701d274c8353833b4f7dc0917d2ee3a45a4eb568
Adrian Roman <aroman@valeo-it.com>
2022-07-06 19:07:38 +0300

    added try / except to the Device class

--- device.py
+++ device.py
@@ -1,34 +1,53 @@
 class Device():
-    cheapest = None
+    _cheapest = None
 
-    def __init__(self,tip, firma, pret) -> None:
-        self.tip = tip
-        self.firma = firma
-        self.pret = pret
-        if Device.cheapest is None:
-            Device.cheapest = pret
+    def __init__(self, tip, firma, pret) -> None:
+        self._tip = tip
+        self._firma = firma
+        self._pret = pret
+        if Device._cheapest is None:
+            Device._cheapest = pret
                    
-        elif pret < Device.cheapest:
-            Device.cheapest = pret
+        elif pret < Device._cheapest:
+            Device._cheapest = pret
 
     def describe(self):
         print("Acesta este un {}, produs de {}, cu pretul de {} de lei.")
 
     def apply_discount(self, percentage):
-        self.percentage = percentage
-        result = (percentage * self.pret) / 100
-        final_result = self.pret - result
-        self.pret = round(final_result)
-        return round(self.pret)
+        self._percentage = percentage
+        result = (percentage * self._pret) / 100
+        final_result = self._pret - result
+        self._pret = round(final_result)
+        return round(self._pret)
 
     @classmethod
     def get_lowest_price(cls):
-         print("Cel mai mic pret este {}".format(Device.cheapest))
+         print("Cel mai mic pret este {}".format(Device._cheapest))
+
+    @property
+    def pret(self):
+        return self._pret
+
+    @pret.setter
+    def pret(self, value):
+        self.value = value
+        
+        try:
+            self._pret = int(value)
+            print("Pretul a fost actualizat cu succes!")
+            
+        except:
+            print("Aceasta valoare nu poate fi transformata in int!")
 
 
 unu = Device("televizor", "panasonic", 4200)
 doi = Device("telefon", "samsung", 2000)
 trei = Device("monitor", "asus", 700)
+
+trei.pret = "500"
+
+doi.pret = "20a"
 
 unu.apply_discount(10)
 doi.apply_discount(30)
